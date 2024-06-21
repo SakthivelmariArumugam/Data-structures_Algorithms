05b197fa8452fb3e8b53a344fec99ee33e2bc4b7
Sakthivelamari <138536342+SakthivelmariArumugam@users.noreply.github.com>
2024-06-22 01:19:30 +0530

    Add files via upload

---
+++ Bubble_Sort.java
@@ -0,0 +1,22 @@
+public class Bubble_Sort {
+    public static void main(String args[])
+    {
+        int a[]={67,21,22,445,5};
+        for(int i=0;i<a.length;i++)
+        {
+            for(int j=0;j<a.length-1-i;j++)
+            {
+                if(a[j]>a[j+1])
+                {
+                    int temp=a[j];
+                    a[j]=a[j+1];
+                    a[j+1]=temp;
+                }
+            }
+        }
+        for(int i:a)
+        {
+            System.out.print(i+" ");
+        }
+    }
+}
